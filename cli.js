1b08da0d63a167e1840913e8e70c78fdf54097f6
Nik Rowell <nik@nikrowell.com>
2021-09-03 16:26:27 -0600

    replace webpack with esbuild

--- cli.js
+++ cli.js
@@ -1,135 +1,125 @@
 #!/usr/bin/env node
-const webpack = require('webpack');
+const esbuild = require('esbuild');
 const path = require('path');
-const SourceMapDevToolPlugin = require('webpack').SourceMapDevToolPlugin;
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const exec = require('child_process').execSync;
+const styles = require('esbuild-sass-plugin').sassPlugin;
+const qrcode = require('qrcode-terminal');
+const chalk = require('chalk');
+const bs = require('browser-sync').create();
 
 const cwd = process.cwd();
-const command = process.argv[2] || 'build';
 const package = require(`${cwd}/package.json`);
+const command = process.argv[2];
 
-const config = {
-  entry: {
-    admin: path.resolve(cwd, 'src/admin'),
-    main: path.resolve(cwd, 'src'),
-  },
-  output: {
-    path: path.resolve(cwd, 'build'),
-    filename: '[name].js',
-  },
-  resolve: {
-    alias: {'@': path.resolve(cwd, 'src')}
-  },
-  devtool: false,
-  module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {presets: [['@babel/preset-env', {targets: '> 0.5%, not IE 11, not dead'}]]}
-      }
-    }, {
-      test: /\.(css|scss)$/,
-      use: [
-        {loader: MiniCssExtractPlugin.loader, options: {publicPath: ''}},
-        {loader: 'css-loader'},
-        {loader: 'sass-loader'},
-      ]
-    }, {
-      test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|json|mp3|mp4|ico)$/,
-      type: 'asset/resource',
-      generator: {emit: false, filename: '../[path][name][ext]?[hash]'}
-    }]
-  }
-};
-
-const watchConfig = Object.assign({}, config, {
-  name: 'watch',
-  mode: 'development',
-  stats: {
-    all: false,
-    colors: true,
-    builtAt: true,
-    timings: true,
-    warnings: true,
-    errors: true,
-  },
-  plugins: [
-    new SourceMapDevToolPlugin(),
-    new MiniCssExtractPlugin({
-      filename: '[name].css'
-    }),
-    new BrowserSyncPlugin({
-      proxy: `${package.name}.test`,
-      host: 'localhost',
-      port: 5000,
-      open: false,
-      notify: false,
-      files: ['build/main.js', 'images/*', '**/*.php']
-    })
-  ]
-});
-
-const buildConfig = Object.assign({}, config, {
-  name: 'build',
-  mode: 'production',
-  output: {
-    path: path.resolve(cwd, 'build'),
-    filename: '[name].min.js',
-  },
-  stats: 'none',
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].min.css'
-    })
-  ]
-});
-
-function callback(err, stats) {
-  if (err) {
-    console.error(err.stack || err);
-    if (err.details) {
-      console.error(err.details);
-    }
-    return;
-  }
-  const info = stats.toJson();
-  if (stats.hasErrors()) {
-    console.error(info.errors);
-  }
-  if (stats.hasWarnings()) {
-    console.warn(info.warnings);
-  }
-  // Log result...
+if (!['build', 'watch'].includes(command)) {
+  console.log(`Invalid command: ${chalk.red(command)}\n`);
+  process.exit();
 }
 
-const compiler = webpack([
-  watchConfig,
-  buildConfig
-]);
+exec(`rm -rf ${cwd}/build/*`);
 
-switch (command) {
+const assets = (options = {}) => ({
+  name: 'assets',
+  setup(build) {
+    build.onResolve({filter: /..\/(fonts|images)\//}, args => ({
+      path: args.path,
+      external: true
+    }))
+  },
+});
 
-  case 'watch':
-    compiler.watch({
-      // https://webpack.js.org/configuration/watch/#watchoptions
-      aggregateTimeout: 300,
-      poll: undefined
-    }, callback);
-    break;
-
-  case 'build':
-    compiler.run((err, stats) => {
-      // https://webpack.js.org/api/node/#stats-object
-      // https://webpack.js.org/configuration/stats/
-      console.log(stats.toJson());
-      // callback(err, stats);
-      compiler.close();
+const errors = (options = {}) => ({
+  name: 'errors',
+  setup(build) {
+    build.onEnd(result => {
+      if (result.errors.length) {
+        console.log(result.errors)
+      }
     });
-    break;
+  },
+});
 
-  default:
-    console.log(`Command "${command}" not found`);
+function entryPoints(suffix = '') {
+
+  if (!suffix.startsWith('.')) {
+    suffix = `.${suffix}`;
+  }
+
+  const entries = Object.entries({
+    admin: path.resolve(cwd, './src/admin'),
+    main: path.resolve(cwd, './src'),
+  });
+
+  return entries.reduce((result, [name, path]) => {
+    result[`${name}${suffix}`] = path;
+    return result;
+  }, {});
+}
+
+const defaults = {
+  watch: command === 'watch',
+  outdir: path.resolve(cwd, 'build'),
+  bundle: true,
+  minify: true,
+  sourcemap: false,
+  logLevel: 'warning',
+  legalComments: 'none',
+  target: 'es2019',
+  plugins: [
+    styles({cache: true}),
+    assets(),
+  ],
+};
+
+const watchConfig = Object.assign({}, defaults, {
+  entryPoints: entryPoints('dev'),
+  minify: false,
+  sourcemap: 'inline',
+  logLevel: 'silent',
+  plugins: [
+    ...defaults.plugins,
+    errors()
+  ],
+});
+
+const buildConfig = Object.assign({}, defaults, {
+  entryPoints: entryPoints('min'),
+});
+
+esbuild.build(watchConfig);
+esbuild.build(buildConfig);
+
+if (command === 'watch') {
+
+  const watchedBuilds = Object.keys(watchConfig.entryPoints).map(name => (
+    `${watchConfig.outdir}/${name}.*`
+  ));
+
+  const files = [
+    ...watchedBuilds,
+    'images/*',
+    '**/*.php'
+  ];
+
+  const options = {
+    proxy: `${package.name}.test`,
+    host: 'localhost',
+    open: false,
+    notify: false,
+    logLevel: 'silent',
+    ui: false,
+    files
+  };
+
+  bs.init(options, () => {
+
+    const host = bs.getOption('proxy').get('target');
+    const port = bs.getOption('port');
+    const proxying = `${host}:${port}`;
+    const external = bs.getOption('urls').get('external');
+
+    external && qrcode.generate(external, {small: true}, console.log);
+    console.log(`Proxying: ${chalk.green(proxying)}`);
+    console.log(`External: ${chalk.cyan(external || 'offline')}`);
+  });
 }
